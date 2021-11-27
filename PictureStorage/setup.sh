217c175c2021d691b023ec2902e083cb5a405aa5
OKKyu <kyukyuinskt@gmail.com>
2021-11-27 10:51:22 +0000

    Upgrade dependency packages version.
    Modify setup.sh : It can choose to with test package or without it.

--- PictureStorage/setup.sh
+++ PictureStorage/setup.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
-mkdir venv log data
+if [ -d ./venv ]; then
+  rm -rf ./venv/.*
+  rm -rf ./venv/*
+  rmdir ./venv
+fi
+if [ ! -d ./venv ]; then
+ mkdir venv
+fi
+if [ ! -d ./log ]; then
+ mkdir log
+fi
+if [ ! -d ./data ]; then
+ mkdir data
+fi
+
 python3 -m venv venv
 source ./venv/bin/activate
-pip install -r requirements.txt
+pip install -U pip
+
+echo "Please input '0' if you want to use only webapp."
+echo "If you want to use test script, please input other char."
+read choose
+if [ "$choose" == "0" ]; then
+ pip install -r requirements.txt
+else
+ pip install -r requirements2.txt
+fi
 deactivate
