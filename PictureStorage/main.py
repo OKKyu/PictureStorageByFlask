5ca3cf981ee26cfc03b4d761f6a8d0fbab28c65e
OKKyu <kyukyuinskt@gmail.com>
2021-11-27 10:30:03 +0000

    main.py : Adding debuglog
    run.sh : Adding timeout option in gunicorn

--- PictureStorage/main.py
+++ PictureStorage/main.py
@@ -8,9 +8,25 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 #app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif', 'jpeg', 'bmp']
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif', '.jpeg', '.bmp']
 app.config['UPLOAD_PATH'] = 'data'
 
+@app.before_request
+def before_request():
+    app.logger.debug("before request....")
+    
+    app.logger.debug("-get data body-")
+    app.logger.debug(request.get_data())
+    
+    app.logger.debug("-request forms key and values-")
+    for key, value in request.form.items():
+        app.logger.debug(key + "  " + value)
+    
+    app.logger.debug("-request headers-")
+    for key, value in request.headers.items():
+        app.logger.debug(key + "  " + value)
+    
+@app.route("/", methods=["POST"])
 @app.route("/pic_post_form", methods=["POST"])
 def pic_post_form():
     app.logger.info('pic_post_form start. request was sended from ' + request.remote_addr)
@@ -20,13 +36,13 @@ def pic_post_form():
           validation check HttpRequest
         '''
         if request.files.get('picture',None) is None:
-            app.logger.warn('Nothing uploaded file.')
+            app.logger.warning('Nothing uploaded file.')
             return app.make_response(jsonify({'result':'Nothing uploaded file. Please send file.'})) , 400
         
         pic = request.files['picture']
         fileName = pic.filename
         if '' == fileName:
-            app.logger.warn('Filename is not inputed')
+            app.logger.warning('Filename is not inputed')
             return app.make_response(jsonify({'result':'Filename is not inputed. Please input filename'})) , 400
         
         '''
@@ -63,7 +79,7 @@ def pic_post_form():
 if __name__ == '__main__':
     #using log.conf for develop mode.
     logging.config.fileConfig(fname='env/log_develop.conf', disable_existing_loggers=False)
-    app.run(host='192.168.1.24', port=5002)
+    app.run(host='192.168.1.53', port=5001)
 else:
     #using log.conf for deploy mode.
     logging.config.fileConfig(fname='env/log_honban.conf', disable_existing_loggers=False)
--- PictureStorage/run.sh
+++ PictureStorage/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source ./venv/bin/activate
-gunicorn -w $1 -b $2:$3 main:app --certfile=env/certfile.crt --keyfile=env/secret_PS.key
+gunicorn -w $1 -b $2:$3 main:app --timeout 240 --certfile=env/certfile.crt --keyfile=env/secret_PS.key
+#gunicorn -w $1 -b $2:$3 main:app --timeout 240
